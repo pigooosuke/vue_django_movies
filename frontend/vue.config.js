3c8a76bed3a773644d4406af391fcf32918043d8
pigooosuke <yusuke.saito@binc.jp>
2019-05-29 19:47:22 +0900

    add vue.config

---
+++ frontend/vue.config.js
@@ -0,0 +1,21 @@
+//
+// vue.confg.js
+//
+
+module.exports = {
+    devServer: {
+      proxy: {
+        '^/(login|logout|social|graphql|admin|static/admin)': {
+          target: 'http://localhost:8000',
+          changeOrigin: true
+        }
+      }
+    },
+    configureWebpack: {
+      devtool: 'source-map'
+    },
+  
+    outputDir: 'dist/',
+    assetsDir: 'static/',
+    indexPath: 'index.html',
+  }
