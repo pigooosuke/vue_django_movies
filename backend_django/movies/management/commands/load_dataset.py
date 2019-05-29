7381b9d471e498842a9575ad7ef72193c2aa08dd
pigooosuke <yusuke.saito@binc.jp>
2019-05-29 10:46:27 +0900

    modified commands

--- backend_django/movies/management/commands/load_dataset.py
+++ backend_django/movies/management/commands/load_dataset.py
@@ -5,6 +5,9 @@ from ...models import Genre, Movie, User, Tag, UserTag, Rating
 import pandas as pd
 import numpy as np
 from itertools import chain
+import pathlib
+import urllib.request
+import zipfile
 
 # BaseCommandを継承して作成
 
@@ -12,11 +15,43 @@ from itertools import chain
 class Command(BaseCommand):
     help = 'Add movie lens dataset'
 
+    movielens_url = 'http://files.grouplens.org/datasets/movielens/'
+    movielens_filename = "ml-latest-small.zip"
+    movielens_extracted_folder = "ml-latest-small"
+    download_dir = pathlib.Path('movies/data')
+
+    def download(self, base_url, filename, download_dir):
+        save_path = download_dir / filename
+        if not save_path.exists():
+            if not download_dir.exists():
+                download_dir.mkdir(exist_ok=True)
+
+            print("Downloading", filename, "...")
+
+            # Download the file from the internet.
+            url = base_url + filename
+            file_path, _ = urllib.request.urlretrieve(url=url,
+                                                      filename=str(save_path))
+
+            print("Download finished. Extracting files.")
+
+            if file_path.endswith(".zip"):
+                # Unpack the zip-file.
+                zipfile.ZipFile(file=file_path, mode="r").extractall(
+                    download_dir)
+
+            print("Done.")
+
     def handle(self, *args, **options):
+        self.download(self.movielens_url, self.movielens_filename, self.download_dir)
+        movies_csv = self.download_dir / self.movielens_extracted_folder / "movies.csv"
+        ratings_csv = self.download_dir / self.movielens_extracted_folder / "ratings.csv"
+        tags_csv = self.download_dir / self.movielens_extracted_folder / "tags.csv"
+
         # load movies, genres
         movies = list()
         genres = list()
-        df_movies = pd.read_csv("movies.csv")
+        df_movies = pd.read_csv(str(movies_csv))
         splited_genres = df_movies['genres'].str.split('|')
         df_movies_repeat = pd.DataFrame({
             'movieId': df_movies['movieId'].values.repeat(splited_genres.str.len()),
@@ -43,8 +78,8 @@ class Command(BaseCommand):
         Through.objects.bulk_create(through_list)
 
         # load ratings, tags
-        df_ratings = pd.read_csv("ratings.csv")
-        df_tags = pd.read_csv("tags.csv")
+        df_ratings = pd.read_csv(str(ratings_csv))
+        df_tags = pd.read_csv(str(tags_csv))
         # user
         users = list()
         user_ids = np.unique(np.concatenate(
@@ -59,7 +94,7 @@ class Command(BaseCommand):
         ratings = list()
         for user_id, movie_id, rating, timestamp in zip(df_ratings["userId"].values, df_ratings["movieId"].values, df_ratings["rating"].values, df_ratings["timestamp"].values):
             rating = Rating(user_id=user_id, movie_id=movie_id,
-                          rating=rating, timestamp=timestamp)
+                            rating=rating, timestamp=timestamp)
             ratings.append(rating)
         # bulk create ratings
         Rating.objects.bulk_create(ratings)
@@ -74,10 +109,12 @@ class Command(BaseCommand):
 
         tag_labels, _ = pd.factorize(df_tags["tag"])
 
+        # user-tags
         usertags = []
         for user_id, movie_id, tag_id, timestamp in zip(df_tags["userId"].values, df_tags["movieId"].values, tag_labels, df_tags["timestamp"].values):
-            usertags.append(UserTag(user_id=user_id, tag_id=tag_id+1, movie_id=movie_id, timestamp=timestamp))
+            usertags.append(UserTag(user_id=user_id, tag_id=tag_id+1,
+                                    movie_id=movie_id, timestamp=timestamp))
         # bulk create movie-genre relation
         UserTag.objects.bulk_create(usertags)
 
-        print("loaded dataset")
+        print("command finished")
